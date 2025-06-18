baa5e6977f870b449f96713e58cb87f2208ef8c7
Abhishek Kumar Singh <abhiits4@gmail.com>
2025-06-18 18:14:50 +0530

    Update ViewSchedule.jsx

--- TMS-frontend/src/components/ViewSchedule.jsx
+++ TMS-frontend/src/components/ViewSchedule.jsx
@@ -12,7 +12,7 @@ const ViewSchedule = () => {
 
       try {
         const res = await fetch(
-          `http://localhost:8080/schedule/appointment?email=${userEmail}`
+          `https://tms-backend-g0yl.onrender.com/schedule/appointment?email=${userEmail}`
         );
         const data = await res.json();
         if (data.success) {
